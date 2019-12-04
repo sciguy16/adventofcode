0a740e12b04861d102dff3e73e2664a601ffd6a3
David Young <david.young@nccgroup.com>
2019-12-04 16:07:17 +0000

    Solves the fourth puzzle

---
+++ 2015/4/src/main.rs
@@ -0,0 +1,52 @@
+use md5::{Md5, Digest};
+
+fn main() {
+    println!("Hello, world!");
+
+    let key = "yzbqklnj";
+
+    //let ans = solve_a(key);
+    //println!("Answer is: {}", ans);
+
+    let ans = solve_b(key);
+    println!("Answer to 2 is: {}", ans);
+}
+
+#[allow(unused)]
+fn solve_a(input: &str) -> i32 {
+    let mut secret: i32 = 0;
+    loop {
+        let mut hasher = Md5::new();
+        hasher.input(format!("{}{}", input, secret));
+        let res = hasher.result();
+        if res[0] == 0 && res[1] == 0 && res[2] < 0x10 {
+            break;
+        }
+        secret += 1;
+    }
+    secret
+}
+
+fn solve_b(input: &str) -> i32 {
+    let mut secret: i32 = 0;
+    loop {
+        let mut hasher = Md5::new();
+        hasher.input(format!("{}{}", input, secret));
+        let res = hasher.result();
+        if res[0] == 0 && res[1] == 0 && res[2] == 0 {
+            break;
+        }
+        secret += 1;
+    }
+    secret
+}
+
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test_input() {
+        let ans = super::solve_a("abcdef");
+        assert_eq!(ans, 609043);
+    }
+}
