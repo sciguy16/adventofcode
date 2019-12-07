bcd5c512b8a915b0bdd43e09d36c9daf0ab6794b
David Young <david@thedavidyoung.co.uk>
2019-12-07 16:50:05 +0000

    Work towards the seventh puzzle

---
+++ 2015/7/src/main.rs
@@ -0,0 +1,30 @@
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn main() {
+    let filename = "test.txt";
+    let wires: HashMap<&str, u16> = HashMap::new();
+
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        let line = line.unwrap();
+        println!("Line is: {}", line);
+        let chunks: Vec<&str> = line.split(" -> ").collect();
+
+        // There should always be a LHS and a RHS
+        assert_eq!(chunks.len(), 2);
+        let lhs = chunks[0];
+        let rhs = chunks[1];
+
+        // Time to resolve the logic operation...
+        match lhs {
+            op if op.contains("AND") => println!("AND!!"),
+            _ => println!("Not and"),
+        }
+
+    }
+    println!("Done!");
+}
