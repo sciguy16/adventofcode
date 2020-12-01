534af023beeefd5dbfcd688fe5f0cd7bdf67da12
David Young <david@thedavidyoung.co.uk>
2020-12-01 10:55:00 +0000

    Some stuff

--- 2019/10/src/main.rs
+++ 2019/10/src/main.rs
@@ -88,9 +88,22 @@ fn main() {
     println!("Can be seen: {:?}", can_be_seen(z, z, &map));
 }
 
-fn count_visible(origin: Point, map: &Map) -> usize {
+fn get_all_counts(map: &Map) -> Array2<usize> {
+    let mut counts = Array2::from_elem(map.asteroids.raw_dim(), 0_usize); 
+    for (point, val) in map.asteroids.indexed_iter() {
+        counts[[point.0, point.1]] = if *val {
+            count_visible(point, &map)
+        } else {
+            0
+        };
+    }
+    counts
+}
+
+fn count_visible<T>(origin: T, map: &Map) -> usize
+    where T: Into<Point> + Copy {
     map.asteroids.indexed_iter().fold(0, |acc, x| {
-        acc + match can_be_seen(origin, (x.0).into(), &map) {
+        acc + match can_be_seen(origin.into(), (x.0).into(), &map) {
             true => 1,
             false => 0,
         }
@@ -122,7 +135,8 @@ fn can_be_seen(origin: Point, target: Point, map: &Map) -> bool {
     let y2 = target.1 as f32;
     let m: f32 = (y1 - y2) / (x1 - x2);
     let line = |x: usize| -> f32 {
-        m*((x - origin.0) as f32) + origin.1 as f32
+        let x = x as f32;
+        m*(x - (origin.0 as f32)) + origin.1 as f32
     };
     for x in (min(origin.0, target.0) + 1)..max(origin.0, target.0) {
         println!("x: {}, y: {}", x, line(x));
@@ -171,7 +185,29 @@ mod test {
 
         let count = count_visible(Point(0, 1), &map);
         assert_eq!(count, 7);
+    }
 
+    #[test]
+    fn test_2() {
+        let map_str =
+            ".#..#
+            .....
+            #####
+            ....#
+            ...##";
+        let height = map_str.lines().count();
+        let width = map_str.lines().next().unwrap().trim().len();
+        let map = Map::new(
+            map_str,
+            width,
+            height,
+            );
+
+        println!("Map: {:?}", map);
+        println!("Formatted properly:\n{}", map);
+
+        println!("All counts:\n{:?}", get_all_counts(&map));
         panic!();
     }
+
 }
