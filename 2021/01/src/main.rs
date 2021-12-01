df7cd8fc73b067f03fdc062a1aed4c2e540cb014
David Young <david@thedavidyoung.co.uk>
2021-12-01 12:43:40 +0000

    Solve 2021/01

---
+++ 2021/01/src/main.rs
@@ -0,0 +1,79 @@
+fn part_one(depths: &[usize]) -> usize {
+    let mut count_increases = 0;
+
+    for pair in depths.windows(2) {
+        if pair[1] > pair[0] {
+            count_increases += 1;
+        }
+    }
+
+    count_increases
+}
+
+fn part_two(depths: &[usize]) -> usize {
+    let mut count_increases = 0;
+
+    for pair in depths
+        .windows(3)
+        .map(|triple| triple.iter().sum())
+        .collect::<Vec<usize>>() // eww, but apparently only slices have windows
+        .windows(2)
+    {
+        if pair[1] > pair[0] {
+            count_increases += 1;
+        }
+    }
+
+    count_increases
+}
+
+fn main() {
+    println!("Hello, world!");
+    let depths_str = include_str!("../input.txt");
+    let depths = depths_str
+        .lines()
+        .map(|line| line.parse::<usize>().unwrap())
+        .collect::<Vec<_>>();
+
+    let ans = part_one(&depths);
+    println!("Part one: {}", ans);
+
+    let ans = part_two(&depths);
+    println!("Part two: {}", ans);
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    fn test_depths() -> Vec<usize> {
+        let depths_str = r#"199
+200
+208
+210
+200
+207
+240
+269
+260
+263"#;
+        depths_str
+            .lines()
+            .map(|line| line.parse::<usize>().unwrap())
+            .collect()
+    }
+
+    #[test]
+    fn test_part_1() {
+        let depths = test_depths();
+        let ans = part_one(&depths);
+        assert_eq!(ans, 7);
+    }
+
+    #[test]
+    fn test_part_2() {
+        let depths = test_depths();
+        let ans = part_two(&depths);
+        assert_eq!(ans, 5);
+    }
+}
