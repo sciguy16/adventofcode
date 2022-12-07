585788dc55026ce0d95d31c34deee217203da5b9
David Young <david@thedavidyoung.co.uk>
2022-12-07 21:44:20 +0000

    Optimise from 800µs to 700µs by changing filenames from String to &str

--- 2022/07/src/main.rs
+++ 2022/07/src/main.rs
@@ -1,17 +1,16 @@
 use color_eyre::{eyre::eyre, Result};
 use std::collections::HashMap;
-use std::str::FromStr;
 
 const THRESHOLD: usize = 100000;
 const FS_SIZE: usize = 70000000;
 const SPACE_NEEDED: usize = 30000000;
 
 #[derive(Debug, Default)]
-struct Directory {
-    inner: HashMap<String, DirEntry>,
+struct Directory<'a> {
+    inner: HashMap<&'a str, DirEntry<'a>>,
 }
 
-impl Directory {
+impl<'a> Directory<'a> {
     pub fn calculate_sizes(&self, sizes: &mut Vec<usize>) -> usize {
         let size = self.inner.values().map(|v| v.calculate_sizes(sizes)).sum();
         sizes.push(size);
@@ -21,12 +20,12 @@ impl Directory {
 }
 
 #[derive(Debug)]
-enum DirEntry {
+enum DirEntry<'a> {
     File { size: usize },
-    Dir { entries: Directory },
+    Dir { entries: Directory<'a> },
 }
 
-impl Default for DirEntry {
+impl Default for DirEntry<'_> {
     fn default() -> Self {
         DirEntry::Dir {
             entries: Default::default(),
@@ -34,7 +33,7 @@ impl Default for DirEntry {
     }
 }
 
-impl DirEntry {
+impl DirEntry<'_> {
     pub fn file(size: usize) -> Self {
         Self::File { size }
     }
@@ -53,10 +52,8 @@ impl DirEntry {
     }
 }
 
-impl FromStr for Directory {
-    type Err = color_eyre::Report;
-
-    fn from_str(inp: &str) -> std::result::Result<Self, Self::Err> {
+impl<'a> Directory<'a> {
+    fn from_str(inp: &'a str) -> Result<Directory<'a>> {
         let mut fs = Directory::default();
 
         let mut path = Vec::new();
@@ -103,17 +100,16 @@ impl FromStr for Directory {
                 let mut current = &mut fs;
                 for level in &path {
                     let DirEntry::Dir{entries,  }  =
-                        current.inner.entry(level.to_string()).or_default()else{panic!()};
+                        current.inner.entry(level).or_default()else{panic!()};
                     current = entries;
                 }
 
                 if first_part == "dir" {
-                    current.inner.insert(name.to_string(), DirEntry::dir());
+                    current.inner.insert(name, DirEntry::dir());
                 } else {
-                    current.inner.insert(
-                        name.to_string(),
-                        DirEntry::file(first_part.parse()?),
-                    );
+                    current
+                        .inner
+                        .insert(name, DirEntry::file(first_part.parse()?));
                 }
             }
         }
@@ -162,7 +158,7 @@ fn solve(inp: &Directory) -> (usize, usize) {
 fn main() -> Result<()> {
     color_eyre::install()?;
     let input = include_str!("../input.txt");
-    let data = input.parse()?;
+    let data = Directory::from_str(input)?;
     // dbg!(&data);
     let (ans1, ans2) = solve(&data);
     println!("part one: {ans1}");
@@ -200,7 +196,7 @@ $ ls
 
     #[test]
     fn test_part_1() {
-        let inp = TEST_DATA.parse().unwrap();
+        let inp = Directory::from_str(TEST_DATA).unwrap();
         dbg!(&inp);
         let (ans, _) = solve(&inp);
         assert_eq!(ans, 95437);
@@ -208,7 +204,7 @@ $ ls
 
     #[test]
     fn test_part_2() {
-        let inp = TEST_DATA.parse().unwrap();
+        let inp = Directory::from_str(TEST_DATA).unwrap();
         let (_, ans) = solve(&inp);
         assert_eq!(ans, 24933642);
     }
